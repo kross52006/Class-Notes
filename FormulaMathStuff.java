ac7e29af12266800dfb41235f9724b1dd0ea91b2
Kylee <kross5@daltonstate.edu>
2026-01-22 13:20:59 -0500

    Add files via upload

---
+++ FormulaMathStuff.java
@@ -0,0 +1,31 @@
+public class FormulaMathStuff
+{
+   public static void main(String[] args)
+   {
+      System.out.println(3 + 7);
+      System.out.println(3 - 7);
+      System.out.println(3 * 7);
+      System.out.println(10 / 3);
+         //.0 makes java look at it differently, its a double number.
+         //3=int 3.0=double
+      System.out.println(3.0 + 7.0);
+      System.out.println(3.0 - 7.0);
+      System.out.println(3.0 * 7.0);  
+         //interger division
+      System.out.println(10 / 3);    
+      System.out.println(10.0 / 3.0);
+         //% is remainder
+      System.out.println(2 % 7);
+      System.out.println(179 % 4);
+      System.out.println(10 % 3);
+      System.out.println();
+         //multiple operations
+      System.out.println(7 + 5 * 3);
+      System.out.println( ( 7 + 5 ) * 3);
+      System.out.println(7 + 5 / 3);
+      System.out.println(7 % 5 + 3);
+      System.out.println(196 % 43 * 8 / 8);
+         //Int,long,float,double
+      System.out.println(3 * 7.0);
+   }   
+}
