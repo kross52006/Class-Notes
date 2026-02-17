6ba9e3cc953b214618f73b56e65ee76d329026a6
Kylee <kross5@daltonstate.edu>
2026-02-17 13:40:54 -0500

    Update LitersAndCentimeters.java

--- LitersAndCentimeters.java
+++ LitersAndCentimeters.java
@@ -8,13 +8,13 @@ public class LitersAndCentimeters
       System.out.println("an integer.");
       int number = meow.nextInt();
       cmToIn(number);
+      literToGal(number);
    }
    
    public static void cmToIn(int inches)
    {
       double cm = inches * 2.54;
       System.out.printf("There are %,.1f cm in %,d in.%n", cm, inches);
-      literToGal(inches);
    }
    
    public static void literToGal(int gallons)
